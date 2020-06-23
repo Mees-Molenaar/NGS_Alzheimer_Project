4c7f02cacac9a42efc120b3eb6f81664a14ec70f
Mees-Molenaar <molen.mees@gmail.com>
2020-06-23 12:06:32 +0200

    Add trimming with seqtk

---
+++ seqtk.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# Default directory to store data is in the data folder, otherwise you can give your directory with -d flag
+WORKDIR="data/"
+
+while getopts "d:" opt; do
+    case $opt in
+        d) WORKDIR=$OPTARG  ;;
+        *) echo 'error' >&2
+            exit 1
+    esac
+done
+
+if [ ! -d $WORKDIR ]; then
+    echo "Could not find working directory: $WORKDIR, exiting. Please make sure the working directory exists"
+    exit 1
+fi
+
+cd $WORKDIR/data/sra
+
+mkdir -p trimmed
+
+for i in *.fastq; do
+    /home/mees/NGS_Alzheimer/seqtk/seqtk trimfq -b 10 $i > trimmed/$i
+done
