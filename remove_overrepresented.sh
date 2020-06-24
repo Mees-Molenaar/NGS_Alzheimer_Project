729eefa06034eeb12abe04e36db77a81ac4ecb21
Mees-Molenaar <molen.mees@gmail.com>
2020-06-24 10:49:50 +0200

    Removed trimming with seqtk and added adapter removal with cutadapt

---
+++ remove_overrepresented.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# Default directory to store data is in the data folder, otherwise you can give your directory with -d flag
+WORKDIR="data/"
+
+while getopts "d:" opt; do
+    case $opt in
+        d) WORKDIR=$OPTARG  ;;
+        *) echo 'error' >&2
+            exit 1
+    esac
+done
+
+if [ ! -d $WORKDIR ]; then
+    echo "Could not find working directory: $WORKDIR, exiting. Please make sure the working directory exists"
+    exit 1
+fi
+
+cd $WORKDIR
+
+for i in *.fastq; do
+    cutadapt -a TCCTGTACTGAGCTGCCCCG -m 18 -o rm_over_$i $i
+done
+
